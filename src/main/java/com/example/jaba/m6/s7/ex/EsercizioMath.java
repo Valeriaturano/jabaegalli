b8725090411678292c393f4a02565bf81f47eabf
Valeria <turanovaleria16@gmail.com>
2024-10-03 15:13:20 +0200

    soluzione esercizio

---
+++ src/main/java/com/example/jaba/m6/s7/ex/EsercizioMath.java
@@ -0,0 +1,16 @@
+package com.example.jaba.m6.s7.ex;
+
+public class EsercizioMath {
+	public static void main(String[] args) {
+		int x = 42;
+		System.out.println("x is" + x);
+		int y = 53;
+		System.out.println(" y is" + y);
+		//TODO: put in ma the largest between x and y
+		int max=Math.max(x, y);
+		System.out.println("Max is" + max);
+		int min=Math.min(x, y);
+		System.out.println("Min is" + min);
+	}
+
+}
